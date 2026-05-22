b65cb6ee87b9377f5e4867d9f17bac4d212b9dfb
temevh <temehama@gmail.com>
2026-05-22 14:31:22 +0300

    initial commit

---
+++ monitand-frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import Main from "./components/Main.vue";
+</script>
+
+<template>
+  <v-app>
+    <v-main>
+      <Main />
+    </v-main>
+  </v-app>
+</template>
---
+++ monitand-frontend/src/components/Main.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { ref, watch } from "vue";
+
+const keyword = ref("");
+watch(keyword, () => {
+  console.log(keyword.value);
+});
+</script>
+
+<template>
+  <v-container class="d-flex align-center justify-center fill-height">
+    <div class="glow-wrapper">
+      <v-text-field
+        label="Search something..."
+        variant="solo"
+        rounded="xl"
+        density="default"
+        class="custom-glow-input"
+        hide-details
+        v-model="keyword"
+      >
+      </v-text-field>
+    </div>
+  </v-container>
+</template>
+
+<style scoped>
+.glow-wrapper {
+  width: 100%;
+  max-width: 650px;
+  padding: 20px;
+}
+
+.custom-glow-input :deep(.v-field) {
+  font-size: 2rem;
+  transition: box-shadow 0.3s ease-in-out;
+  box-shadow: 0 0 120px rgba(33, 150, 243, 0.4);
+}
+
+.custom-glow-input :deep(.v-field__input) {
+  min-height: 80px !important;
+  padding-top: 12px !important;
+  padding-bottom: 12px !important;
+}
+
+.custom-glow-input :deep(.v-field--focused) {
+  box-shadow: 0 0 200px rgb(238, 158, 11) !important;
+}
+</style>
